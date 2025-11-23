9d08f971923a014f512a604a75b958dbe3307062
Tejas Gouda <tejasgouda26@email.com>
2025-11-23 10:42:17 +0530

    added problem no 860  in arrays

---
+++ Arrays/860. Lemonade Change.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool lemonadeChange(vector<int>& bills) {
+        
+        int count_5 = 0 , count_10 = 0;
+        for(int i=0; i<bills.size(); i++){
+            if(bills[i]==5){
+                count_5++;
+            }
+            else if(bills[i] == 10 && count_5 > 0){
+                count_10++;
+                count_5--;
+            }
+            else if(bills[i]==20 && ((count_10 > 0 && count_5>0)|| count_5 >= 3)){
+                if(count_10 > 0 && count_5>0){
+                    count_10--;
+                    count_5--;
+                }
+                else if(count_5 >= 3)
+                    count_5 -= 3;
+            }
+            else{
+                return false;
+            }
+        }
+        return true;
+    }
+};
