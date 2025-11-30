32e411347f7ea490b19115a167b634e45cc6df13
Tejas Gouda <tejasgouda26@email.com>
2025-11-30 21:38:29 +0530

    added problem no 35 in binary search

---
+++ CODEFORCES/1.cpp
@@ -0,0 +1,13 @@
+#include<bits/std.h>
+using namespace std;
+int main(){
+    int N;
+    vector<int> A;
+    cin>>N;
+    for(int i=0;i<N;i++){
+        int a;
+        cin>>a;
+        A.push_back(a);
+    }
+    
+}
