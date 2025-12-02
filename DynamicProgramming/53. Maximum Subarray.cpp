f6aafd25d1a12e408e7d64d1672fda1c611ccaab
Tejas Gouda <tejasgouda26@email.com>
2025-12-02 15:10:18 +0530

    added problem no 53 in dynamic prograamming

---
+++ DynamicProgramming/53. Maximum Subarray.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int cur_sum=0,max_sum=INT_MIN;
+        for(int x : nums){
+            cur_sum+=x;
+            max_sum = max(max_sum,cur_sum);
+            if(cur_sum<0){
+                cur_sum = 0;
+            }
+        }
+        return max_sum;
+    }
+};
