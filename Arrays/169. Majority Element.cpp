8620cb975abd6de8008b20056a0445b2b425012f
Tejas Gouda <tejasgouda26@email.com>
2025-12-03 13:48:07 +0530

    added optimised solution to problem number 169

--- Arrays/169. Majority Element.cpp
+++ Arrays/169. Majority Element.cpp
@@ -5,3 +5,24 @@ public:
         return nums[nums.size() / 2];
     }
 };
+
+
+// moore's voting algorithm implementation
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int freq = 0, ans ;
+        for(int i=0;i<nums.size();i++){
+            if(freq == 0){
+                ans = nums[i];
+            }
+            if( ans == nums[i]){
+                freq++;
+            }
+            else{
+                freq--;
+            }
+        }
+        return ans;
+    }
+};
